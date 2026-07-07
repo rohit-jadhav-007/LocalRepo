5c1b040120a654ae0cca5b2438387434b097b60b
rohit-jadhav-oo7 <rohit.jadhav010404@gmail.com>
2026-07-07 12:58:37 +0530

    Upto date

--- Index.java
+++ Index.java
@@ -2,7 +2,6 @@ public class Index {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("This is the Index class in the localRepo in feature branch.");
-        System.out.println("This is the Index class in the localRepo in feature one branch.");
 
         System.out.println("hi");
     }
